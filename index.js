88023bdec64734db8636c7138a6fd881922322d6
Abednego <abedikroos15@gmail.com>
2023-03-23 17:44:42 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Select the main element with ID "main"
+const main = document.querySelector('#main');
+
+// Remove the main element from the DOM
+main.remove();
+
+// Create a new H1 element with ID "victory"
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+// Add text to the new H1 element
+const name = "YOUR-NAME";
+newHeader.textContent = `${name} is the champion`;
+
+// Append the new H1 element to the body of the document
+document.body.appendChild(newHeader);
